5b62a503f8e8ff90b56c7e6c4a7eb3420fe93fd8
Cigan Oliviu David <ciganoliviudavid@gmail.com>
2024-01-09 19:25:49 +0200

    Update sketch

--- src/components/WhatWeDo/WhatWeDo.css.tsx
+++ src/components/WhatWeDo/WhatWeDo.css.tsx
@@ -4,16 +4,31 @@ import { Breakpoints, minWidthQuery } from "@components/utils/cssMedia";
 export const WhatWeDoWrapper = styled.div`
     background: #fff;
     color: #000;
-    display: flex;
-    flex-wrap: wrap;
-    padding: 0 6rem 0 6rem;
+    padding: 0 12rem 0 12rem;
 `;
 
-export const WhatWeDoColumn = styled.div<{ isLastElement: boolean }>`
+export const WhatWeDoTitle = styled.div`
+    padding: 2rem;
+    font-size: 3rem;
+    font-weight: bold;
+`;
+
+export const WhatWeDoContent = styled.div`
+    font-size: 1rem;
+    padding: 2rem;
+`;
+
+
+export const WhatWeDoFlexContainer = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+`;
+
+export const WhatWeDoColumn = styled.div<{ columnPercentage: number, shouldHaveBorder?: boolean }>`
     flex: 1 1 100%; 
-    border-right: ${(props) => props.isLastElement ? 'none' : '1px solid rgba(204, 204, 204, 0.7)'};
+    border-right: ${(props) => props.shouldHaveBorder ? '1px solid rgba(204, 204, 204, 0.7)' : 'none'};
   
     ${minWidthQuery(Breakpoints.medium)} {
-        flex: 1 1 25%;
+        flex: ${(props) => `1 1 ${props.columnPercentage}%;`}
     }
 `;
--- src/components/WhatWeDo/WhatWeDo.tsx
+++ src/components/WhatWeDo/WhatWeDo.tsx
@@ -1,20 +1,43 @@
 import React, { FC } from "react";
-import { WhatWeDoColumn, WhatWeDoWrapper } from "@components/components/WhatWeDo/WhatWeDo.css";
+import {
+    BreathingSpace,
+    WhatWeDoColumn, WhatWeDoContent,
+    WhatWeDoFlexContainer,
+    WhatWeDoTitle,
+    WhatWeDoWrapper
+} from "@components/components/WhatWeDo/WhatWeDo.css";
 import { ServiceCard } from "@components/components/ServiceCard/ServiceCard";
 import { WhatWeDoConfig } from "@components/configs/general";
+import { Separator } from "@components/components/GeneralStyleSheet/GeneralStyleSheet";
 
 export const WhatWeDo: FC = () => {
     return (
         <WhatWeDoWrapper>
-            {WhatWeDoConfig.map((service: ServiceType, index) => {
-                const isLastElement = WhatWeDoConfig.length - 1 === index;
-
-                return (
-                    <WhatWeDoColumn key={service.title} isLastElement={isLastElement}>
-                        <ServiceCard service={service} />
+            <BreathingSpace>
+                <WhatWeDoFlexContainer>
+                    <WhatWeDoColumn columnPercentage={45}>
+                        <WhatWeDoTitle>Take your craft to the next level</WhatWeDoTitle>
                     </WhatWeDoColumn>
-                );
-            })}
+                    <WhatWeDoColumn columnPercentage={55}>
+                        <WhatWeDoContent>
+                            <p>Adipiscing elit, sed do euismod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco</p>
+                            <Separator paddingValue={1} />
+                            <p>Adipiscing elit, sed do euismod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
+                        </WhatWeDoContent>
+                    </WhatWeDoColumn>
+                </WhatWeDoFlexContainer>
+            </BreathingSpace>
+            <WhatWeDoFlexContainer>
+                {WhatWeDoConfig.map((service: ServiceType, index) => {
+                    const isNotTheLastElement = WhatWeDoConfig.length - 1 !== index;
+
+                    return (
+                        <WhatWeDoColumn key={service.title} columnPercentage={25} shouldHaveBorder={isNotTheLastElement}>
+                            <ServiceCard service={service} />
+                        </WhatWeDoColumn>
+                    );
+                })}
+            </WhatWeDoFlexContainer>
         </WhatWeDoWrapper>
     );
 };
