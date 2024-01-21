c327afb7e2f588c923289f73ded798c78facbf4e
Cigan Oliviu David <ciganoliviudavid@gmail.com>
2024-01-21 22:59:28 +0200

    Updates

--- src/components/ContactForm/ContactForm.tsx
+++ src/components/ContactForm/ContactForm.tsx
@@ -1,4 +1,4 @@
-import { FC } from "react";
+import React, { FC, useEffect, useState } from "react";
 import {
     ContactWrapper, ContactHeader,
     ContactInput, ContactParagraph,
@@ -11,7 +11,71 @@ import {
 } from "@components/components/GeneralStyleSheet/GeneralStyleSheet";
 import { ContactConfig, formSubmitCodeUrl } from "@components/configs/general";
 
+enum EmailElements {
+    NAME = 'name',
+    EMAIL = 'email',
+    MESSAGE = 'message'
+}
+
+const isValidEmail = (email: string): boolean => {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+};
+
 export const ContactForm: FC = () => {
+    const [name, setName] = useState<string>('');
+    const [email, setEmail] = useState<string>('');
+    const [message, setMessage] = useState<string>('');
+    const [canSubmitForm, setCanSubmitForm] = useState<boolean>(false);
+    const [wasFormDataSubmitted, setWasFormDataSubmitted] = useState<boolean>(false);
+
+    const isNameFieldValid = (): boolean => {
+        return !(name.trim() === '');
+    };
+
+    const isEmailFieldValid = (): boolean => {
+        return !(email.trim() === '' || !isValidEmail(email.trim()));
+    };
+
+    const isMessageFieldValid = (): boolean => {
+        return !(message.trim() === '');
+    };
+
+    const handleInputNameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setName(event.target.value);
+    };
+
+    const handleInputEmailChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setEmail(event.target.value);
+    };
+
+    const handleInputMessageChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+        setMessage(event.target.value);
+    };
+
+    const onSubmitFormProcess = () => {
+        setWasFormDataSubmitted(true);
+    };
+
+    const clearFormInputs = () => {
+        setName('');
+        setEmail('');
+        setMessage('');
+    };
+
+    useEffect(() => {
+        setCanSubmitForm(isNameFieldValid() && isEmailFieldValid() && isMessageFieldValid())
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [name, email, message]);
+
+    useEffect(() => {
+        if (wasFormDataSubmitted) {
+            clearFormInputs();
+            setWasFormDataSubmitted(false);
+        }
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [wasFormDataSubmitted]);
+
     return (
         <ContactWrapper>
             <FlexContainer>
@@ -26,16 +90,28 @@ export const ContactForm: FC = () => {
                     </ContactTextBox>
                 </FlexColumn>
                 <FlexColumn columnPercentage={75}>
-                    <ContactFormWrapper action={formSubmitCodeUrl} method="POST">
-                        <ContactInput placeholder={'name'} name={'name'} />
+                    <ContactFormWrapper action={formSubmitCodeUrl} method="POST" onSubmit={onSubmitFormProcess}>
+                        <ContactInput
+                            placeholder={EmailElements.NAME}
+                            name={EmailElements.NAME}
+                            onChange={handleInputNameChange}
+                        />
                         <SeparatorSpace paddingValue={1} />
-                        <ContactInput placeholder={'email'} type={'email'} name={'email'}/>
+                        <ContactInput
+                            placeholder={EmailElements.EMAIL}
+                            type={EmailElements.EMAIL}
+                            name={EmailElements.EMAIL}
+                            onChange={handleInputEmailChange}
+                        />
                         <SeparatorSpace paddingValue={1} />
-                        <ContactInput placeholder={'subject'} name={'subject'}/>
+                        <ContactTextArea
+                            rows={12}
+                            placeholder={EmailElements.MESSAGE}
+                            name={EmailElements.MESSAGE}
+                            onChange={handleInputMessageChange}
+                        />
                         <SeparatorSpace paddingValue={1} />
-                        <ContactTextArea rows={12} placeholder={'message'} name={'message'} />
-                        <SeparatorSpace paddingValue={1} />
-                        <Button>Send Message</Button>
+                        <Button disabled={!canSubmitForm}>Send Message</Button>
                     </ContactFormWrapper>
                 </FlexColumn>
                 <SeparatorSpace paddingValue={4} />
