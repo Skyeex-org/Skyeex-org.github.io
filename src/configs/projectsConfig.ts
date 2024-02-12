5314bc4d7601feff18204df2a3729b51aceba320
Richard-Daniel Vass <vassrichard09@yahoo.com>
2024-02-13 00:03:59 +0200

    Added projects content

--- src/configs/projectsConfig.ts
+++ src/configs/projectsConfig.ts
@@ -50,45 +50,34 @@ export const ProjectsConfig: ProjectsConfigType[] = [
     {
         id: 'cruze',
         title: 'Cruze',
-        subtitle: 'An usecase driven approach',
+        subtitle: 'Mobile corporate carpooling application',
         icon: Cruze.src,
         previewDescription: 'Cruze revolutionizes commuting with its mobile carpooling app specially tailored for businesses and institutions fostering a sense for efficient and sustainable transportation.',
         htmlField: {
-            __html: 'During my time at the faculty, I had the opportunity to delve into a wide range of ' +
-                'courses across various disciplines. <div>&nbsp;</div> In the field of Mathematics, I took courses such as Linear ' +
-                'Algebra and Analytical Geometry, Mathematical Analysis (Differential Calculus), Mathematical ' +
-                'Analysis (Integral Calculus and Differential Equations), Special Mathematics in Engineering, and ' +
-                'Numerical Calculus. These courses played a fundamental role in shaping my understanding of complex ' +
-                'problems and honing my analytical skills, both of which are important in programming and systems theory.\n' +
-                '<div>&nbsp;</div>In Systems Engineering, I gained valuable insights into the inner workings of modern systems and ' +
-                'learned efficient methods for their analysis and control. Process Modeling allowed me to ' +
-                'mathematically model mechanical and electrical systems, providing a solid foundation for ' +
-                'understanding their behavior. The Signals and Systems course equipped me with the ability ' +
-                'to comprehend both discrete and continuous signals, enabling me to analyze and recognize ' +
-                'patterns effectively. This course also delved into advanced topics like image processing and ' +
-                'mathematical concepts like the Fourier transformation for modeling. Systems Theory provided a ' +
-                'comprehensive understanding of how systems behave in diverse environments, offering insights into ' +
-                'analysis, behavior prediction, and methods for understanding complex systems. Systems Identification, ' +
-                'on the other hand, focused on pattern recognition and the ability to identify models for specific ' +
-                'systems based on input-output data. I acquired proficiency in various identification methods, ' +
-                'from parametrical to non-parametrical, and gained skills in evaluating model accuracy. Lastly, ' +
-                'Automatic Control Engineering involved analyzing systems with predefined performance criteria ' +
-                'and implementing control strategies to enhance system performance.\n' +
-                '<div>&nbsp;</div>In Electronics & Engineering, I explored hardware design, hardware analysis, ' +
-                'firmware development, and data transmission, broadening my knowledge of electronics and embedded systems.\n' +
-                '<div>&nbsp;</div>In the realm of Software Engineering, my coursework covered a diverse array of topics. I delved ' +
-                'into the intricacies of the Linux Operating System, gained proficiency in programming algorithms, ' +
-                'and developed a deep understanding of Object-Oriented Programming and Design. I also delved into ' +
-                'threads management and concurrent programming, explored SQL-based databases, and honed my skills ' +
-                'in web backend and frontend development. Furthermore, I gained experience in assembly programming ' +
-                'and learned to build graphical models using Lisp and functional programming.\n' +
-                'In summary, my educational journey at the faculty provided me with a well-rounded knowledge base ' +
-                'that spans mathematics, systems engineering, electronics, and software engineering. These courses' +
-                ' not only enriched my technical expertise but also fostered critical thinking and problem-solving ' +
-                'abilities, preparing me for a wide range of challenges and opportunities in the field of engineering ' +
-                'and technology.',
+            __html: 'Unlock a new era of workplace connectivity with this innovative platform designed to foster camaraderie, cut costs and save time. ' +
+            'Say goodbye to the hassle of solo rides and hello to a greener, more efficient way of traveling.' +
+            '<div>&nbsp;</div>' +
+            '<span style="font-size: 1.8rem">Notable Features:</span>' +
+            '<div>&nbsp;</div>' +
+            '<li><b>Simple & intuitive interface</b> - Navigate the app effortlessly with its user-friendly design, making carpooling a breeze.</li>' +
+            '<li><b>Join or create rides</b> - Easily locate and connect with colleagues traveling on similar routes or take control and set your own route. Switch between being a driver and a passenger, providing flexibility for all commuting needs.</li>' +
+            '<li><b>Flexible pick-up & drop-off options</b> - Customize your commuting experience by setting your preferred points – whether it is your starting point, final destination or somewhere along the route.</li>' +
+            '<li><b>Google & Waze integration</b> - Access places of reference or real-time traffic updates and optimized routes for seamless travel to your destination.</li>' +
+            '<li><b>Reward system</b> - Earn rewards based on participation, encouraging sustainable transportation choices and fostering community engagement.</li>' +
+            '<li><b>Personalized profile settings</b> - Easily manage your settings, update your information, and tailor your experience to suit your individual needs.</li>' +
+            '<div>&nbsp;</div>' +
+            '<span style="font-size: 1.8rem">Benefits:</span>' +
+            '<div>&nbsp;</div>' +
+            '<li><b>Convenience</b> - Save time and hassle by finding rides with coworkers, reducing the stress of commuting.</li>' +
+            '<li><b>Cost reduction</b> - Lower parking expenses and decrease the need for company-provided transportation services.</li>' +
+            '<li><b>Pollution reports</b> - As a company you benefit from CO2 reports at need about the environmental impact and carbon footprint.</li>' +
+            '<li><b>Environmental impact</b> - Reduce carbon emissions by carpooling, contributing to a greener and more sustainable future.</li>' +
+            '<li><b>Networking opportunities</b> - Build relationships and strengthen professional connections with colleagues during shared rides.</li>' +
+            '<li><b>Gifts & rewards</b> - Drive or participate and get rewarded with gas vouchers or discounts at partner businesses.</li>' +
+            '<div>&nbsp;</div>' +
+            "Ready to transform your workplace commute? Elevate your team's travel experience with this carpooling mobile application – where connectivity, sustainability, and collaboration converge for a brighter, more efficient future! 🚀"
         },
-        isDetailPageEnabled: false,
+        isDetailPageEnabled: true,
     },
     {
         id: 'car-damage-detector',
@@ -136,45 +125,32 @@ export const ProjectsConfig: ProjectsConfigType[] = [
     {
         id: 'adda',
         title: 'ADDA',
-        subtitle: 'An usecase driven approach',
+        subtitle: 'Artificial vision for defect detection in the ceramic industry',
         icon: Adda.src,
         previewDescription: 'Unveiling ADDA (Automated defect detection analyser) where the convergence of machine learning and neural networks meet, paving the way for unparalleled precision in quality control processes.',
         htmlField: {
-            __html: 'During my time at the faculty, I had the opportunity to delve into a wide range of ' +
-                'courses across various disciplines. <div>&nbsp;</div> In the field of Mathematics, I took courses such as Linear ' +
-                'Algebra and Analytical Geometry, Mathematical Analysis (Differential Calculus), Mathematical ' +
-                'Analysis (Integral Calculus and Differential Equations), Special Mathematics in Engineering, and ' +
-                'Numerical Calculus. These courses played a fundamental role in shaping my understanding of complex ' +
-                'problems and honing my analytical skills, both of which are important in programming and systems theory.\n' +
-                '<div>&nbsp;</div>In Systems Engineering, I gained valuable insights into the inner workings of modern systems and ' +
-                'learned efficient methods for their analysis and control. Process Modeling allowed me to ' +
-                'mathematically model mechanical and electrical systems, providing a solid foundation for ' +
-                'understanding their behavior. The Signals and Systems course equipped me with the ability ' +
-                'to comprehend both discrete and continuous signals, enabling me to analyze and recognize ' +
-                'patterns effectively. This course also delved into advanced topics like image processing and ' +
-                'mathematical concepts like the Fourier transformation for modeling. Systems Theory provided a ' +
-                'comprehensive understanding of how systems behave in diverse environments, offering insights into ' +
-                'analysis, behavior prediction, and methods for understanding complex systems. Systems Identification, ' +
-                'on the other hand, focused on pattern recognition and the ability to identify models for specific ' +
-                'systems based on input-output data. I acquired proficiency in various identification methods, ' +
-                'from parametrical to non-parametrical, and gained skills in evaluating model accuracy. Lastly, ' +
-                'Automatic Control Engineering involved analyzing systems with predefined performance criteria ' +
-                'and implementing control strategies to enhance system performance.\n' +
-                '<div>&nbsp;</div>In Electronics & Engineering, I explored hardware design, hardware analysis, ' +
-                'firmware development, and data transmission, broadening my knowledge of electronics and embedded systems.\n' +
-                '<div>&nbsp;</div>In the realm of Software Engineering, my coursework covered a diverse array of topics. I delved ' +
-                'into the intricacies of the Linux Operating System, gained proficiency in programming algorithms, ' +
-                'and developed a deep understanding of Object-Oriented Programming and Design. I also delved into ' +
-                'threads management and concurrent programming, explored SQL-based databases, and honed my skills ' +
-                'in web backend and frontend development. Furthermore, I gained experience in assembly programming ' +
-                'and learned to build graphical models using Lisp and functional programming.\n' +
-                'In summary, my educational journey at the faculty provided me with a well-rounded knowledge base ' +
-                'that spans mathematics, systems engineering, electronics, and software engineering. These courses' +
-                ' not only enriched my technical expertise but also fostered critical thinking and problem-solving ' +
-                'abilities, preparing me for a wide range of challenges and opportunities in the field of engineering ' +
-                'and technology.',
+            __html: 'Revolutionize your quality control process with ADDA, a groundbreaking solution that harnesses the power of machine learning and neural networks to bring unparalleled accuracy to defect detection in dinnerware. ' +
+            '<div>&nbsp;</div>' +
+            '<span style="font-size: 1.8rem">Notable Features:</span>' +
+            '<div>&nbsp;</div>' +
+            '<li><b>Image pre-processing</b> - Efficiently pre-process 2D grayscale images to extract and isolate object borders, ensuring accurate defect analysis.</li>' +
+            '<li><b>Defect detection</b> - Utilize advanced machine learning algorithms to analyze extracted borders and identify any defects present, enhancing quality control processes.</li>' +
+            '<li><b>Adaptability</b> - Easily handle both circular and non-circular objects commonly found in the dinnerware industry, providing versatile defect detection capabilities.</li>' +
+            '<li><b>Integration capabilities</b> - Integrate with existing production workflows and systems, minimizing disruption and maximizing efficiency.</li>' +
+            '<li><b>Consistent quality</b> - Ensure consistent quality standards across production batches by leveraging automated defect detection, mitigating variations in inspection outcomes due to human error.</li>' +
+            '<div>&nbsp;</div>' +
+            '<span style="font-size: 1.8rem">Benefits:</span>' +
+            '<div>&nbsp;</div>' +
+            '<li><b>Enhanced quality control:</b> - Improve the accuracy and effectiveness of quality control processes by automating defect detection on object borders, reducing the likelihood of faulty products reaching consumers.</li>' +
+            '<li><b>Increased efficiency</b> - Streamline production workflows by automating defect analysis, allowing for quicker identification and resolution of issues without compromising throughput.</li>' +
+            '<li><b>Cost savings</b> - Minimize the need for manual inspection and rework by detecting defects early in the production process, resulting in reduced waste and improved resource utilization.</li>' +
+            '<li><b>Market competitiveness</b> - Maintain a competitive edge in the market by delivering high-quality products that meet or exceed customer expectations, enhancing brand reputation and customer satisfaction.</li>' +
+            '<li><b>Data-driven insights</b> - Gain valuable insights into production quality and defect trends through detailed analysis of defect data, informing continuous improvement efforts and decision-making.</li>' +
+            '<li><b>ROI maximization</b> - Achieve a strong return on investment by leveraging automated defect detection to reduce costs, improve product quality, and enhance overall operational performance.</li>' +
+            '<div>&nbsp;</div>' +
+            'Experience the future of defect detection with ADDA – where machine learning, neural networks, and advanced imaging converge to elevate your quality control standards to unprecedented heights! 🌟'
         },
-        isDetailPageEnabled: false,
+        isDetailPageEnabled: true,
     },
 ];
 
