bc3fd53d073decf4c759dd0229b5897aefb579ec
CiganOliviu <ciganoliviudavid@gmail.com>
2024-03-18 19:48:07 +0200

    try nextjs config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    trailingSlash: true,
-    output: 'export',
+    reactStrictMode: true,
+
+    compiler: {
+        styledComponents: true
+    }
 }
+
 module.exports = nextConfig
